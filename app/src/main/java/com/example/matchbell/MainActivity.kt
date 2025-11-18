315ee16aa454241051e70302bd48dca6e664f75d
Park Jiwoo <jiwoo4237@naver.com>
2025-11-19 00:01:07 +0900

    Radar 1차 구현 완료, My 1차 구현 완료

--- app/src/main/java/com/example/matchbell/MainActivity.kt
+++ app/src/main/java/com/example/matchbell/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.matchbell// (본인 패키지 이름에 맞게 수정하세요!)
+package com.example.matchbell
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +7,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import android.view.View
+import androidx.core.view.WindowCompat
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -14,26 +15,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // 1. 방금 수정한 껍데기(activity_main.xml)를 화면에 띄움
         setContentView(R.layout.activity_main)
 
-        // 2. 껍데기에서 '탭 바'와 '빈 공간(지도 관리자)'을 찾아옴
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
 
-        // 3. 둘을 연결! (이제 탭을 누르면 화면이 바뀝니다)
+        WindowCompat.setDecorFitsSystemWindows(window, true)
+
         bottomNavView.setupWithNavController(navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
-                // 스플래시와 로그인 화면일 때 탭 바 숨김
                 R.id.splashFragment, R.id.loginFragment, R.id.signupTermsFragment, R.id.signupInfoFragment -> {
                     bottomNavView.visibility = View.GONE
                 }
                 else -> {
-                    // 나머지 탭 화면 (레이더, 매칭 등)에서는 보임
                     bottomNavView.visibility = View.VISIBLE
-                } // ✅ when 구문의 닫는 중괄호를 여기에 추가했습니다.
+                }
             }
         }
     }
--- app/src/main/java/com/example/matchbell/data/model/LoginRequest.kt
+++ app/src/main/java/com/example/matchbell/data/model/LoginRequest.kt
@@ -1,7 +1,5 @@
 package com.example.matchbell.data.model
-
-// 💡 class 앞에 'data' 키워드를 추가합니다.
 data class LoginRequest(
-    val email: String,
+    val id: String,
     val password: String
 )
