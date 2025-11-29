a3b14fe401885c3b5db0098f1d819972d6e7ceaa
kimsye <133931385+kimsye@users.noreply.github.com>
2025-11-29 21:35:24 +0900

    서버 연결

--- app/src/main/java/com/example/matchbell/di/NetworkModule.kt
+++ app/src/main/java/com/example/matchbell/di/NetworkModule.kt
@@ -1,6 +1,6 @@
 package com.example.matchbell.di
 
-import com.example.matchbell.network.AuthApi // (빨간 줄 뜨면 Alt+Enter)
+import com.example.matchbell.network.AuthApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -13,22 +13,23 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    // 1. Retrofit (전화기) 만들기 (기존에 있었어야 할 코드)
+    // [중요] 백엔드 언니가 알려준 서버 주소 (끝에 슬래시 / 필수!)
+    private const val BASE_URL = "http://16.184.9.169:8080/"
+
+    // 1. Retrofit (전화기) 만들기
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            // 에뮬레이터에서 내 컴퓨터(로컬) 서버에 접속할 때 쓰는 주소입니다.
-            .baseUrl("http://10.0.2.2:8080/")
+            .baseUrl(BASE_URL) // 위에서 설정한 진짜 서버 주소 사용
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-    // 2. AuthApi (전화번호부) 만들기 (이번에 추가하는 코드)
+    // 2. AuthApi (전화번호부) 만들기
     @Provides
     @Singleton
     fun provideAuthApi(retrofit: Retrofit): AuthApi {
         return retrofit.create(AuthApi::class.java)
     }
-
 }
